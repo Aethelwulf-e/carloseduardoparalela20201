2acaa0b02877814003b4c6cdbf0e4b98dfac3d6c
Carlos Eduardo da Silva Ferreira <43002604+Aethelwulf-e@users.noreply.github.com>
2020-08-04 23:23:46 -0300

    Create parallel_sum_a.c

---
+++ atividades/atividade03/parallel_sum_a.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main (int argc , char *argv[]) {
+    int max;
+    sscanf (argv[1], "%d", &max);
+    int ts = omp_get_max_threads();
+    int r = max % ts;
+	int sums[ts];
+	#pragma omp parallel
+	{
+		int t = omp_get_thread_num ();
+		int lo, hi;
+		if(t < r) {
+			lo = (max / ts) * (t + 0) + 1 + t;
+			hi = (max / ts) + lo;
+		} else {
+			lo = (max / ts) * (t + 0) + 1 + r;
+			hi = (max / ts) + lo - 1;
+		}
+		sums[t] = 0;
+		for (int i = lo; i <= hi; i++)
+			sums[t] = sums[t] + i;
+	}
+    int sum = 0;
+    for (int t = 0; t < ts; t++) sum = sum + sums[t];
+    printf ("%d\n", sum);
+    return 0;
+}
