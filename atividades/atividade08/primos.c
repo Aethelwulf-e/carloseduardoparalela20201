06910ba60fded55c3558ccde82a37b04bbb6a8a7
Carlos Eduardo da Silva Ferreira <43002604+Aethelwulf-e@users.noreply.github.com>
2020-09-08 01:47:17 -0300

    Arrumando indentação que o Github buga

--- atividades/atividade08/primos.c
+++ atividades/atividade08/primos.c
@@ -30,49 +30,49 @@ int main(int argc, char *argv[]) {
     int chunk_size = 10000;
     
     if(rank == 0) {
-		int *is_prime = (int*)malloc(n * sizeof(int));
+	int *is_prime = (int*)malloc(n * sizeof(int));
 		
-		for(int i = 0; i < n/chunk_size; i++) {
-			int snd = i * chunk_size;
+	for(int i = 0; i < n/chunk_size; i++) {
+		int snd = i * chunk_size;
 			
-			MPI_Send(&snd, 1, MPI_INT, get_random_number(1, size-1, i), 0, MPI_COMM_WORLD);
-		}
+		MPI_Send(&snd, 1, MPI_INT, get_random_number(1, size-1, i), 0, MPI_COMM_WORLD);
+	}
 		
-		for(int i = 0; i < n/chunk_size; i++) {
-			int source_rank = get_random_number(1, size-1, i);
-			MPI_Recv(is_prime + (i * chunk_size), chunk_size, MPI_INT, source_rank, 0, MPI_COMM_WORLD, &status);
-		}
+	for(int i = 0; i < n/chunk_size; i++) {
+		int source_rank = get_random_number(1, size-1, i);
+		MPI_Recv(is_prime + (i * chunk_size), chunk_size, MPI_INT, source_rank, 0, MPI_COMM_WORLD, &status);
+	}
 		
-		for(int i = 0; i < n; i++) {
-			if(is_prime[i]) {
-				printf("%d, ", i);
-			}
-		}
-		printf("\n");
-		
-		for(int i = 1; i < size; i++) {
-			int snd = -1;
-			MPI_Send(&snd, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-		}
-		
-	} else {
-		while(1) {
-			int inicio;
-			MPI_Recv(&inicio, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-			
-			if(inicio == -1) {
-				break;
-			}
-			
-			int *is_prime_local = (int*)malloc(chunk_size * sizeof(int));
-		
-			for(int i = 0; i < chunk_size; i++) {
-				is_prime_local[i] = primo(i+inicio);
-			}
-		
-			MPI_Send(is_prime_local, chunk_size, MPI_INT, 0, 0, MPI_COMM_WORLD);
+	for(int i = 0; i < n; i++) {
+		if(is_prime[i]) {
+			printf("%d, ", i);
 		}
 	}
+	printf("\n");
+		
+	for(int i = 1; i < size; i++) {
+		int snd = -1;
+		MPI_Send(&snd, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+	}
+		
+    } else {
+	while(1) {
+		int inicio;
+		MPI_Recv(&inicio, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+			
+		if(inicio == -1) {
+			break;
+		}
+			
+		int *is_prime_local = (int*)malloc(chunk_size * sizeof(int));
+		
+		for(int i = 0; i < chunk_size; i++) {
+			is_prime_local[i] = primo(i+inicio);
+		}
+		
+		MPI_Send(is_prime_local, chunk_size, MPI_INT, 0, 0, MPI_COMM_WORLD);
+	}
+    }
     
     MPI_Finalize();
     return 0;
